8e859288ce545466e677b23193c2b618d2097b61
tusharcu12@gmail.com <tusharcu12@gmail.com>
2022-04-26 23:13:55 -0400

    Add Changes

---
+++ Data/DBSync/dbo/Tables/MessageReply_1.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[MessageReply] (
+    [MessageReplyId] INT            IDENTITY (1, 1) NOT NULL,
+    [Body]           NVARCHAR (MAX) NULL,
+    [MessageId]      INT            NOT NULL,
+    [ActiveStatusId] INT            NOT NULL,
+    [CreatedDate]    NVARCHAR (MAX) NULL,
+    [UpdatedDate]    NVARCHAR (MAX) NULL,
+    [UserId]         INT            NOT NULL,
+    CONSTRAINT [PK_MessageReply] PRIMARY KEY CLUSTERED ([MessageReplyId] ASC),
+    FOREIGN KEY ([MessageId]) REFERENCES [dbo].[Message] ([MessageId]),
+    FOREIGN KEY ([UserId]) REFERENCES [dbo].[User] ([UserId]),
+    CONSTRAINT [FK_MessageReply_Message_MessageId] FOREIGN KEY ([MessageId]) REFERENCES [dbo].[Message] ([MessageId]) ON DELETE CASCADE
+);
+
