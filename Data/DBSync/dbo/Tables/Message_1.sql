4e46ed78955af9db1544ffa12bec5a3136800bb8
tusharcu12@gmail.com <tusharcu12@gmail.com>
2022-04-26 23:13:55 -0400

    Add Changes

---
+++ Data/DBSync/dbo/Tables/Message_1.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Message] (
+    [MessageId]      INT            IDENTITY (1, 1) NOT NULL,
+    [Body]           NVARCHAR (MAX) NULL,
+    [ActiveStatusId] INT            NOT NULL,
+    [CreatedDate]    NVARCHAR (MAX) NULL,
+    [UpdatedDate]    NVARCHAR (MAX) NULL,
+    [UserId]         INT            NOT NULL,
+    CONSTRAINT [PK_Message] PRIMARY KEY CLUSTERED ([MessageId] ASC),
+    CONSTRAINT [FK_Message_User_UserId] FOREIGN KEY ([UserId]) REFERENCES [dbo].[User] ([UserId]) ON DELETE CASCADE
+);
+
