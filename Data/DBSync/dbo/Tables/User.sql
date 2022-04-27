a1842af56dc7617bfe3805737821792da9fe17da
tusharcu12@gmail.com <tusharcu12@gmail.com>
2022-04-26 21:18:56 -0400

    "Add DBSync Project To The Solution"

---
+++ Data/DBSync/dbo/Tables/User.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[User] (
+    [UserId]      INT            IDENTITY (1, 1) NOT NULL,
+    [FirstName]   NVARCHAR (MAX) NULL,
+    [LastName]    NVARCHAR (MAX) NULL,
+    [Email]       NVARCHAR (MAX) NULL,
+    [Password]    NVARCHAR (MAX) NULL,
+    [CreatedDate] NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([UserId] ASC)
+);
+
